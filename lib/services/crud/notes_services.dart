62d7a6f55a793f8343ba0cde4f348a20f5e9113b
unknown <bertayyonel@gmail.com>
2022-08-31 19:04:34 +0300

    step-14

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -155,7 +155,7 @@ class NotesService {
     final results = await db.query(
       userTable,
       limit: 1,
-      where: 'email = =',
+      where: 'email = ?',
       whereArgs: [email.toLowerCase()],
     );
     if (results.isEmpty) {
@@ -171,7 +171,7 @@ class NotesService {
     final results = await db.query(
       userTable,
       limit: 1,
-      where: 'email = =',
+      where: 'email = ?',
       whereArgs: [email.toLowerCase()],
     );
     if (results.isNotEmpty) {
@@ -310,16 +310,20 @@ const emailColumn = 'email';
 const userIDColumn = 'user_id';
 const textColumn = 'text';
 const isSyncedWithCloudColumn = 'is_synced_with_cloud';
-const createUserTable = '''CREATE TABLE IF NOT EXISTS "user" (
+const createUserTable = '''
+CREATE TABLE IF NOT EXISTS "user" (
           "id"	INTEGER NOT NULL,
           "email"	TEXT NOT NULL UNIQUE,
           PRIMARY KEY("id" AUTOINCREMENT)
-        );''';
-const createNoteTable = '''CREATE TABLE "note" (
+        );
+        ''';
+const createNoteTable = '''
+CREATE TABLE IF NOT EXISTS "note" (
           "id"	INTEGER NOT NULL,
           "user_id"	INTEGER NOT NULL,
           "text"	TEXT,
           "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
           PRIMARY KEY("id" AUTOINCREMENT),
           FOREIGN KEY("user_id") REFERENCES "user"("id")
-        );''';
+        );
+        ''';
